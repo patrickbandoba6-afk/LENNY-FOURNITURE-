35678f48f089fbe538deb5af801b7d9b8d4a08ce
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:14:54 +0200

    Sauvegarde automatique 2026-09-14 13:14:54

---
+++ app/(student)/cours.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function StudentCoursesScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Cours"
+      description="Emploi du temps détaillé par matière, enseignant et salle."
+    />
+  );
+}
