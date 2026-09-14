081dd3b993f3c45a435522fe6e33b5363a5d9b8f
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:15:06 +0200

    Sauvegarde automatique 2026-09-14 13:15:06

---
+++ app/(teacher)/index.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function TeacherHomeScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Accueil"
+      description="Vue d'ensemble de tes classes, devoirs à corriger et annonces récentes."
+    />
+  );
+}
