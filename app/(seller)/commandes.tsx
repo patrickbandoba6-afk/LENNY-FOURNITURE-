2612a6ba3eb48a019e5302aaf2664e218bc88875
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:15:28 +0200

    Sauvegarde automatique 2026-09-14 13:15:28

---
+++ app/(seller)/commandes.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function SellerOrdersScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Commandes"
+      description="Suivi des commandes reçues et de leur statut de livraison."
+    />
+  );
+}
