8cead932c9bd176aec671d0668d4ae49bf961cf7
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:14:55 +0200

    Sauvegarde automatique 2026-09-14 13:14:55

---
+++ app/(student)/devoirs.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function StudentHomeworkScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Devoirs"
+      description="Liste des devoirs à faire, en cours, terminés et en retard."
+    />
+  );
+}
