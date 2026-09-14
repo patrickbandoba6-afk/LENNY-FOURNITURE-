7fac2078d49d55acb53af68998b3e82d6397cce7
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:15:26 +0200

    Sauvegarde automatique 2026-09-14 13:15:26

---
+++ app/(seller)/produits.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function SellerProductsScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Produits"
+      description="Gestion du catalogue : prix, stock et photos."
+    />
+  );
+}
