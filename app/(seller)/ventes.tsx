77b10c447ea596aae62970179e9a591fe5300913
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:15:29 +0200

    Sauvegarde automatique 2026-09-14 13:15:29

---
+++ app/(seller)/ventes.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function SellerSalesScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Ventes"
+      description="Historique des ventes et paiements reçus."
+    />
+  );
+}
