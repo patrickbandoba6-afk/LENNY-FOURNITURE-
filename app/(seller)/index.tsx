dea19d901f7631c77da71441ea54e1679615c5cb
Patrick Bandoba <patrickbandoba6@gmail.com>
2026-09-14 15:15:25 +0200

    Sauvegarde automatique 2026-09-14 13:15:25

---
+++ app/(seller)/index.tsx
@@ -0,0 +1,10 @@
+import { RolePlaceholderScreen } from "@/components/RolePlaceholderScreen";
+
+export default function SellerDashboardScreen() {
+  return (
+    <RolePlaceholderScreen
+      title="Dashboard"
+      description="Chiffre d'affaires, commandes récentes et alertes de stock."
+    />
+  );
+}
